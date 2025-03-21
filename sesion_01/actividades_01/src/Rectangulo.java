51e0888ef02c715d964cb0dd19d87ae92ed702cf
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-03-20 20:22:46 -0500

    clase rectangulo

--- sesion_01/actividades_01/src/Rectangulo.java
+++ sesion_01/actividades_01/src/Rectangulo.java
@@ -41,3 +41,27 @@ public class Coordenada {
         return"("+ x +"," + y + ")" ;
     }
 }
+public class Rectangulo {
+    private Coordenada esquina1;
+    private Coordenada esquina2;
+    public Rectangulo (Coordenada c1, Coordenada c2){
+        setEsquina1(c1);
+        setEsquina2(c2);
+    }
+    public void setEsquina1 (Coordenada coo){
+
+    }
+    public void setEsquina2 (Coordenada coo){
+
+    }
+    public Coordenada getEsquina1 (){
+
+    }
+    public Coordenada getEsquina2 (){
+
+    }
+    public String toString(){
+        
+    }
+}
+
