67324a88485f3d5d442725c3b30b29b60d79f1e0
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-03-20 19:17:10 -0500

    codigo base

---
+++ sesion_01/actividades_01/src/Coordenada.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Coordenada {
+    private double x;
+    private double y;
+    public Coordenada(){
+
+    }
+    public Coordenada(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public Coordenada(Coordenada c){
+
+    }
+    void setX(double x){
+
+    }
+    void setY(double y){
+
+    }
+    double getX(double x){
+
+    }
+    double getY(double y){
+
+    }
+    double distancia(Coordenada c){
+
+    }
+    static double distancia(Coordenada c1, Coordenada c2){
+
+    }
+    String toString(){
+        
+    }
+}
