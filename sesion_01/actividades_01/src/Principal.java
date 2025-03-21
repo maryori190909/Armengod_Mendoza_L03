56903670fac9ed444fe152e76fadd6efad95df14
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-03-20 23:20:36 -0500

    vr1 main

---
+++ sesion_01/actividades_01/src/Principal.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Ingrese las cordenas del rectangulo1 : ");
+        System.out.println("Esquina1 (x,y) : ");
+    }
+}
