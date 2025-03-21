67167db5dc04c7bc9789a2dd3868f59754a5ee93
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-03-20 19:37:27 -0500

    constructores

--- sesion_01/actividades_01/src/Coordenada.java
+++ sesion_01/actividades_01/src/Coordenada.java
@@ -3,14 +3,16 @@ public class Coordenada {
     private double x;
     private double y;
     public Coordenada(){
-
+        this.x = 0;
+        this.y = 0;
     }
     public Coordenada(double x, double y){
         this.x = x;
         this.y = y;
     }
     public Coordenada(Coordenada c){
-
+        this.x = c.x;
+        this.y = c.y;
     }
     void setX(double x){
 
@@ -31,6 +33,6 @@ public class Coordenada {
 
     }
     String toString(){
-        
+
     }
 }
