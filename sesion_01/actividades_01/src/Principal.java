e92e9bcc0eef5b3c0892a35153a6b207fa042504
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-08 15:41:54 -0500

    esquinas

--- sesion_01/actividades_01/src/Principal.java
+++ sesion_01/actividades_01/src/Principal.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
 
 public class Principal {
-    public static void mostrarRectangulo(String nombre, Rectangulo r) {
-        System.out.println("Rectángulo " + nombre + " = " + r.toString());
+    public static void mostrarRectangulo(String nombre, rectangulo r) {
+        System.out.println("Rectangulo " + nombre + " = " + r.toString());
+    }
+    public static Rectangulo rectanguloInterseccion(rectangulo r1, rectangulo r2) {
+        double xMin = Math.max(r1.getXMin(), r2.getXMin());
+        double xMax = Math.min(r1.getXMax(), r2.getXMax());
+        double yMin = Math.max(r1.getYMin(), r2.getYMin());
+        double yMax = Math.min(r1.getYMax(), r2.getYMax());
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
