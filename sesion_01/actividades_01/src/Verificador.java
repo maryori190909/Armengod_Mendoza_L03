19ea12a08f2e3bd72352b71547741fdbb747c1e0
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-08 13:28:45 -0500

    casos

--- sesion_01/actividades_01/src/Verificador.java
+++ sesion_01/actividades_01/src/Verificador.java
@@ -9,10 +9,15 @@ public class Verificador {
         return true;
     }
 }
-public static boolean esJUnto(rectangulo r1, rectangulo r2){
+public static boolean esJunto(rectangulo r1, rectangulo r2){
     if(esSobrePos(r1,r2)){
     return false;}
     else{
-        
+        boolean tocanVertical = (r1.getXMax() > r2.getXMin() && r1.getXMin() < r2.getXMax()) &&
+        (r1.getYMax() == r2.getYMin() || r1.getYMin() == r2.getYMax());
+        boolean tocanHorizontal = (r1.getYMax() > r2.getYMin() && r1.getYMin() < r2.getYMax()) &&
+        (r1.getXMax() == r2.getXMin() || r1.getXMin() == r2.getXMax());
+        boolean tocanEsquina = (r1.getXMax() == r2.getXMin() || r1.getXMin() == r2.getXMax()) &&
+        (r1.getYMax() == r2.getYMin() || r1.getYMin() == r2.getYMax());
     }
 }
