0d5feb07fa88bd08753bb4610f8a5afa32d5661b
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-10 12:57:42 -0500

    errores2

--- sesion_01/actividades_01/src/Verificador.java
+++ sesion_01/actividades_01/src/Verificador.java
@@ -1,7 +1,7 @@
 public class Verificador {
 
-    public static boolean esSobrePos(rectangulo r1, rectangulo r2){
-        if (r1.getXMAx()<= r2.getMin() || r1.getXMin ()>= r2.getXMax() ||
+    public static boolean esSobrePos(Rectangulo r1, Rectangulo r2){
+        if (r1.getXMAx()<= r2.getXMin() || r1.getXMin ()>= r2.getXMax() ||
         r1.getYMax() <= r2.getYMin() || r1.getYMin() >= r2.getYMax()) {
         return false;
     }
