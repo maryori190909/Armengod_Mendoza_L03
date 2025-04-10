c049b890f697ef48c58eec1c067d0c99b58e4b01
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-10 15:24:09 -0500

    verificador

---
+++ sesion_01/actividades_01/src/Ejercicios01/Verificador.java
@@ -0,0 +1,27 @@
+public class Verificador {
+
+    public static boolean esSobrePos(Rectangulo r1, Rectangulo r2){
+        if (r1.getXMax()<= r2.getXMin() || r1.getXMin ()>= r2.getXMax() ||
+        r1.getYMax() <= r2.getYMin() || r1.getYMin() >= r2.getYMax()) {
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+public static boolean esJunto(Rectangulo r1, Rectangulo r2){
+    if(esSobrePos(r1,r2)){
+    return false;}
+    else{
+        boolean tocanVertical = (r1.getXMax() > r2.getXMin() && r1.getXMin() < r2.getXMax()) &&
+        (r1.getYMax() == r2.getYMin() || r1.getYMin() == r2.getYMax());
+        boolean tocanHorizontal = (r1.getYMax() > r2.getYMin() && r1.getYMin() < r2.getYMax()) &&
+        (r1.getXMax() == r2.getXMin() || r1.getXMin() == r2.getXMax());
+        boolean tocanEsquina = (r1.getXMax() == r2.getXMin() || r1.getXMin() == r2.getXMax()) &&
+        (r1.getYMax() == r2.getYMin() || r1.getYMin() == r2.getYMax());
+        return tocanVertical || tocanHorizontal || tocanEsquina;}
+}
+public static boolean esDisjunto(Rectangulo r1, Rectangulo r2) {
+    return !esSobrePos(r1,r2)&& !esJunto(r1, r2);
+}
+}
