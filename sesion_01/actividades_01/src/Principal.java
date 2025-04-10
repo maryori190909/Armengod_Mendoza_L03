5dfd990e62a75026754af3686b6e4d64ffa99393
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-10 12:44:24 -0500

    opcion1

--- sesion_01/actividades_01/src/Principal.java
+++ sesion_01/actividades_01/src/Principal.java
@@ -44,44 +44,48 @@ public class Principal {
         System.out.println("2 Probar casos automaticos ");
         System.out.print("opcion: ");
 
-        int opcion = scanner.nextInt();
-        System.out.println("Ingrese las cordenas del rectangulo1 : ");
-        System.out.println("Esquina1 (x,y) : ");
-        double x1 = scanner.nextDouble();
-        double y1 = scanner.nextDouble();
-        System.out.println("Esquina2 (x,y) :");
-        double x2 = scanner.nextDouble();
-        double y2 = scanner.nextDouble();
-        
-        Coordenada esquina1A = new Coordenada(x1,y1);
-        Coordenada esquina2A = new Coordenada(x2,y2);
-        Rectangulo A = new Rectangulo(esquina1A, esquina2A);
+        if (opcion == 1) {
 
-        System.out.println("Ingrese las cordenas del rectangulo2 : ");
-        System.out.println("Esquina1 (x,y) : ");
-        double x3 = scanner.nextDouble();
-        double y3 = scanner.nextDouble();
-        System.out.println("Esquina2 (x,y) :");
-        double x4 = scanner.nextDouble();
-        double y4 = scanner.nextDouble();
+            System.out.println("Ingrese las cordenas del rectangulo1 : ");
+            System.out.println("Esquina1 (x,y) : ");
+            double x1 = scanner.nextDouble();
+            double y1 = scanner.nextDouble();
+            System.out.println("Esquina2 (x,y) :");
+            double x2 = scanner.nextDouble();
+            double y2 = scanner.nextDouble();
+            
+            Coordenada esquina1A = new Coordenada(x1,y1);
+            Coordenada esquina2A = new Coordenada(x2,y2);
+            Rectangulo A = new Rectangulo(esquina1A, esquina2A);
 
-        Coordenada esquina1B = new Coordenada(x3,y3);
-        Coordenada esquina2B = new Coordenada(x4,y4);
-        Rectangulo B = new Rectangulo(esquina1B, esquina2B);
-        
-        mostrarRectangulo("rectangulo A", A);
-        mostrarRectangulo("rectangulo B", B);
+            System.out.println("Ingrese las cordenas del rectangulo2 : ");
+            System.out.println("Esquina1 (x,y) : ");
+            double x3 = scanner.nextDouble();
+            double y3 = scanner.nextDouble();
+            System.out.println("Esquina2 (x,y) :");
+            double x4 = scanner.nextDouble();
+            double y4 = scanner.nextDouble();
 
-        if (Verificador.esSobrePos(A, B)) {
-            System.out.println("rectangulos A y B se sobreponen.");
-            Rectangulo interseccion = rectanguloInterseccion(A, B);
-            System.out.printf("area de sobreposición = %.2f%n", interseccion.calculoArea());
+            Coordenada esquina1B = new Coordenada(x3,y3);
+            Coordenada esquina2B = new Coordenada(x4,y4);
+            Rectangulo B = new Rectangulo(esquina1B, esquina2B);
+            
+            mostrarRectangulo("rectangulo A", A);
+            mostrarRectangulo("rectangulo B", B);
 
-        } else if (Verificador.esJunto(A, B)) {
-            System.out.println("rectangulos A y B se juntan.");
-        } else {
-            System.out.println("rectangulos A y B son disjuntos.");
+            if (Verificador.esSobrePos(A, B)) {
+                System.out.println("rectangulos A y B se sobreponen.");
+                Rectangulo interseccion = rectanguloInterseccion(A, B);
+                System.out.printf("area de sobreposición = %.2f%n", interseccion.calculoArea());
+
+            } else if (Verificador.esJunto(A, B)) {
+                System.out.println("rectangulos A y B se juntan.");
+            } else {
+                System.out.println("rectangulos A y B son disjuntos.");
+            }
+
         }
+        
         scanner.close();
 
     }
