7ec775e4df75d6f7bfada089e2972b9c887244bb
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-10 13:31:37 -0500

    cambi2

--- sesion_01/actividades_01/src/Rectangulo.java
+++ sesion_01/actividades_01/src/Rectangulo.java
@@ -15,48 +15,7 @@ public class Rectangulo {
     public Coordenada getEsquina1 (){
         return new Coordenada(esquina1);
     }
-    public Coordenada getEsquina2 (){import java.util.*;
-public class Rectangulo {
-    private Coordenada esquina1;
-    private Coordenada esquina2;
-    public Rectangulo (Coordenada c1, Coordenada c2){
-        setEsquina1(c1);
-        setEsquina2(c2);
-    }
-    public void setEsquina1 (Coordenada coo){
-        this.esquina1 = new Coordenada(coo);
-    }
-    public void setEsquina2 (Coordenada coo){
-        this.esquina2 = new Coordenada(coo);
-    }
-    public Coordenada getEsquina1 (){
-        return new Coordenada(esquina1);
-    }
     public Coordenada getEsquina2 (){
-        return new Coordenada(esquina2);
-    }
-
-    public double getXMin() {
-        return Math.min(esquina1.getX(), esquina2.getX());
-    }
-    public double getXMax() {
-        return Math.max(esquina1.getX(), esquina2.getX());
-    }
-    public double getYMin() {
-        return Math.min(esquina1.getY(), esquina2.getY());
-    }
-    public double getYMax() {
-        return Math.max(esquina1.getY(), esquina2.getY());
-    }
-    public double calculoArea() {
-        return Math.abs((getXMax() - getXMin()) * (getYMax() - getYMin()));
-    }
-    @Override
-    public String toString(){
-        return "El rectangulo tiene coordenada: ([" + getXMin() + ", " + getYMin() + "], [" + getXMax() + ", " + getYMax() + "])";
-    }
-}
-
         return new Coordenada(esquina2);
     }
 
