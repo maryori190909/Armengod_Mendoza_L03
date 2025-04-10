0850f8a6f45bc2f5b1a941198dc413448afa8ca0
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-10 15:30:30 -0500

    constructor

--- sesion_01/actividades_01/src/Ejercicios01/ContainerRect.java
+++ sesion_01/actividades_01/src/Ejercicios01/ContainerRect.java
@@ -5,4 +5,11 @@ public class ContainerRect {
     private int n;
     private static int numRec = 0;
 
+    public ContainerRect(int capacidad){
+        this.n = capacidad;
+        this.rectangulos = new Rectangulo[capacidad];
+        this.distancias = new double[capacidad];
+        this.areas = new double[capacidad];
+    }
+
 }
