e557503dbf198e4c71128d943103eff02456a812
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-10 15:27:01 -0500

    clasecontainer

---
+++ sesion_01/actividades_01/src/Ejercicios01/ContainerRect.java
@@ -0,0 +1,8 @@
+public class ContainerRect {
+    private Rectangulo[] rectangulos;
+    private double[] distancias;
+    private double[] areas;
+    private int n;
+    private static int numRec = 0;
+
+}
